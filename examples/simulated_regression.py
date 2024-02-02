2b375b162df12552309db3b8f66246c4eabb5f6c
devindg <devindg@outlook.com>
2024-02-02 07:10:34 -0600

    Increased level of alpha shading for forecast intervals - Updated pyproject.toml

--- examples/simulated_regression.py
+++ examples/simulated_regression.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     plt.plot(sarima_forecast['mean'])
     plt.fill_between(sarima_forecast.index,
                      sarima_forecast['mean_ci_lower'],
-                     sarima_forecast['mean_ci_upper'], alpha=0.2)
+                     sarima_forecast['mean_ci_upper'], alpha=0.4)
     plt.title('SARIMA: Forecast')
     plt.legend(['Actual', 'Mean', '95% Prediction Interval'])
     plt.show()
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     plt.plot(mle_uc_forecast['mean'])
     plt.fill_between(mle_uc_forecast.index,
                      mle_uc_forecast['mean_ci_lower'],
-                     mle_uc_forecast['mean_ci_upper'], alpha=0.2)
+                     mle_uc_forecast['mean_ci_upper'], alpha=0.4)
     plt.title('MLE UC: Forecast')
     plt.legend(['Actual', 'Mean', '95% Prediction Interval'])
     plt.show()
@@ -133,7 +133,7 @@ if __name__ == '__main__':
 
     plt.plot(y_test)
     plt.plot(forecast_mean)
-    plt.fill_between(np.arange(forecast_mean.size), forecast_l95, forecast_u95, alpha=0.2)
+    plt.fill_between(np.arange(forecast_mean.size), forecast_l95, forecast_u95, alpha=0.4)
     plt.title('Bayesian UC: Forecast')
     plt.legend(['Actual', 'Mean', '95% Prediction Interval'])
     plt.show()
